7cd15ab408e9b49c6b0a0c4788e05dc6d13aea61
Quentin18 <quentindeschamps18@gmail.com>
2024-10-20 14:30:39 +0200

    chore: add max episode steps

--- src/gymnasium_search_race/__init__.py
+++ src/gymnasium_search_race/__init__.py
@@ -3,4 +3,5 @@ from gymnasium.envs.registration import register
 register(
     id="gymnasium_search_race/SearchRace-v0",
     entry_point="gymnasium_search_race.envs:SearchRaceEnv",
+    max_episode_steps=600,
 )
