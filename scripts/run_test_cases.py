bbee046fac1009b7f6d02ddc81504e5ddf9050e2
Quentin18 <quentindeschamps18@gmail.com>
2025-01-19 11:21:09 +0100

    feat: update run test cases to v2

--- scripts/run_test_cases.py
+++ scripts/run_test_cases.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--env",
-        default="gymnasium_search_race:gymnasium_search_race/SearchRace-v1",
+        default="gymnasium_search_race:gymnasium_search_race/SearchRace-v2",
         help="environment id",
     )
     parser.add_argument(
